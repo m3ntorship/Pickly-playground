6f61e98e2730d46ee7bc245e7018733622ce49b8
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-01-17 18:41:53 +0200

    Fix the Divider Height in storybook

--- storybook/src/Sahl/Atoms/Divider/Divider.js
+++ storybook/src/Sahl/Atoms/Divider/Divider.js
@@ -4,9 +4,10 @@ import PropTypes from "prop-types";
 export default function Divider(props) {
   const { className, direction = "horizontal" } = props;
   return (
+    // h-32 added only for purpose of demonestration in storybook. Later it should be h-full
     <div
       className={`bg-grey-shd6 ${
-        direction === "horizontal" ? "w-full h-px" : "h-full w-px"
+        direction === "horizontal" ? "w-full h-px" : "h-32 w-px"
       } ${className}`}
     ></div>
   );
@@ -14,5 +15,5 @@ export default function Divider(props) {
 
 Divider.propTypes = {
   direction: PropTypes.oneOf(["horizontal", "vertical"]),
-  className: PropTypes.string,
+  // className: PropTypes.string,
 };
