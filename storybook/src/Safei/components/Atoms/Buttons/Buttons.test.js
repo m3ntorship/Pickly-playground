55bcd16f804ad394b35bfff8928987e08de01109
Safei-Ashraf <safei.ashraf@gmail.com>
2021-01-20 20:07:08 +0200

    added button react tests on click while disabled

--- storybook/src/Safei/components/Atoms/Buttons/Buttons.test.js
+++ storybook/src/Safei/components/Atoms/Buttons/Buttons.test.js
@@ -1,7 +1,20 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Button from "./Button";
 
+//Testing libr:
+describe("test button comp on click functoinality", () => {
+  it("should return 0 clicks when the button is clicekd while disabled and text passed", () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick} disabled={true} text={"Click Me"} />);
+    userEvent.click(screen.getByText("Click Me"));
+    expect(handleClick).toHaveBeenCalledTimes(0);
+  });
+});
+
+//Snapshot:
 describe("render a primary lg button", () => {
   it("render primary button comp large size", () => {
     const tree = renderer
