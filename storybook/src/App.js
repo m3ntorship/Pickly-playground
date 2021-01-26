7cce5b581566c3cfac75a824991eccc41ea79823
Safei-Ashraf <safei.ashraf@gmail.com>
2021-01-26 11:50:49 +0200

    5

--- storybook/src/App.js
+++ storybook/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Form from "./Safei/DynamicInputTask/Form";
 import ImagesUploader from "./Safei/UploadTask/ImagesUploader";
 const App = () => {
   return (
     <div className="flex flex-col   items-center mt-8 py-16 bg-grey-bg2">
       <ImagesUploader />
+      <Form />
     </div>
   );
 };
