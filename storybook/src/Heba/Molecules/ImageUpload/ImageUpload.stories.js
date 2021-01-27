97887b799edbaaa0ebdd1048b004f3e9703fed89
hebasaadosman <hebaosman443@gmail.com>
2021-01-27 15:35:19 +0300

    remove muli control for file upload

--- storybook/src/Heba/Molecules/ImageUpload/ImageUpload.stories.js
+++ storybook/src/Heba/Molecules/ImageUpload/ImageUpload.stories.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { ImageUpload, IMAGEUPLOAD } from "./ImageUpload";
+import { ImageUpload } from "./ImageUpload";
 
 export default {
   title: "Heba/Molecules/ImageUpload",
   component: ImageUpload,
-  argTypes: {
-    multiple: {
-      control: {
-        type: "radio",
-        options: [IMAGEUPLOAD.MULTIPLE.OFF, IMAGEUPLOAD.MULTIPLE.ON],
-      },
-    },
-  },
+  argTypes: {},
 };
 
 const Template = (args) => <ImageUpload {...args} />;
 export const upload = Template.bind({});
-upload.args = {
-  multiple: IMAGEUPLOAD.MULTIPLE.OFF,
-};
