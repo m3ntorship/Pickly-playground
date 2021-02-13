999bb0e26c48182f83c27590927d82808a913883
hebasaadosman <hebaosman443@gmail.com>
2021-02-13 15:54:55 +0300

    edit

--- storybook/src/RTL-heba/Mock/Currency.js
+++ storybook/src/RTL-heba/Mock/Currency.js
@@ -1,15 +1,15 @@
 async function convert(base, destination) {
-  try {
-    const result = await fetch(
-      `https://api.exchangeratesapi.io/latest?base=${base}`
-    );
-    const data = await result.json();
-    console.log("data",data);
-    return data.rates[destination];
+    try {
+        const result = await fetch(
+            `https://api.exchangeratesapi.io/latest?base=${base}`
+        );
+        const data = await result.json();
+        console.log("data", data);
+        return data.rates[destination];
 
-  } catch (e) {
-    return null;
-  }
+    } catch (e) {
+        return null;
+    }
 }
 
 export { convert };
